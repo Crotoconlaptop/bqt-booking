62753660b7a443575768b0004fb8e791631aa233
Oscar Giordano <98002522+Crotoconlaptop@users.noreply.github.com>
2025-03-20 09:26:01 +0300

    supabase3

--- src/BookingForm.jsx
+++ src/BookingForm.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { supabase } from "../supabaseClient";
+import { supabase } from "@/supabaseClient";
+
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { toast } from "react-toastify";
