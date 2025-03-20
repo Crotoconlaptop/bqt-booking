af02b81267e786f262f3d031d03c4c517a94c373
Oscar Giordano <98002522+Crotoconlaptop@users.noreply.github.com>
2025-03-20 09:23:34 +0300

    supabase2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://jpappufrtwvzgzwkqote.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwYXBwdWZydHd2emd6d2txb3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0NTAzMjcsImV4cCI6MjA1ODAyNjMyN30.XKX41CSp3S1qxFseunBufuFVYZSLaUpW0v_C5Tymnf8";
+// Obtiene las credenciales desde variables de entorno
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// Crea el cliente de Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
